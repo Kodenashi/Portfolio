f4902f448b514de8bd79768392c3bc87ba7f4375
DevSeikean <jancelleopoldo@gmail.com>
2025-06-19 22:06:44 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const path = window.location.pathname.split("/").pop() || "index.html";
+  const links = document.querySelectorAll("nav a");
+
+  links.forEach(link => {
+    if (link.getAttribute("href") === path) {
+      link.classList.add("active");
+    }
+  });
+});
